94b5cd7af8db95b71cd5e7d20eb06842e492fe5e
Xiangy <enerty1011@gmail.com>
2026-03-11 16:00:29 +0000

    vector初始化

--- vector_8.cpp
+++ vector_8.cpp
@@ -1,1 +1,17 @@
-#include 
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void print(const vector<int> &v){
+  for(int elem:v){
+    cout << elem << " ";
+  }
+  cout << '\n';
+}
+
+int main(){
+  vector<int> list;
+  cout << "vector初始化" << '\n';
+  print(list);
+}
